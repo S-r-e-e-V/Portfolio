caed57b05ca43c21d4452113a2fed6c31bdcef5b
Sreekanth V <25sreekanth@gmail.com>
2024-02-09 04:41:47 -0500

    style: image hover animation

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,15 +1,6 @@
 import React, { useRef } from "react";
 import "./LandingPage.css";
 
-import SideNav from "../../components/SideNav";
-import Home from "../../sections/Home";
-import AboutMe from "../../sections/AboutMe";
-import Education from "../../sections/Education";
-import Experience from "../../sections/Experience";
-import Projects from "../../sections/Projects";
-import Awards from "../../sections/Awards";
-import Publications from "../../sections/Publications";
-
 import Router from "../../routes/routes";
 
 export default function LandingPage() {
